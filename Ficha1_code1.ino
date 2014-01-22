7c17d785a2f96af100c6afa30f5bb4f5c852bde7
yayitazale <surf.obsession@gmail.com>
2014-01-22 11:03:44 +0100

    Create Ficha1_code1.ino

---
+++ Ficha1_code1.ino
@@ -0,0 +1,32 @@
+    const int ROJO = 11;      
+    const int AMBAR = 10;         
+    const int VERDE = 9;
+    const int BLANCO = 6;
+
+    
+  
+    void setup()
+    {
+    pinMode(ROJO, OUTPUT);      // pinMode es una función que define un pin como entrada o salida. Ponemos 
+    pinMode(BLANCO, OUTPUT);    // pinMode( [el pin sobre el que queremos actuar], INPUT (entrada) / OUTPUT (salida)
+    pinMode(VERDE, OUTPUT);
+    pinMode(BLANCO, OUTPUT);
+    }
+    
+    
+    void loop()
+     
+    {
+    int Tiempo = 1000;
+      
+      digitalWrite(ROJO, HIGH);
+      digitalWrite(AMBAR, HIGH);
+      digitalWrite(VERDE, HIGH);
+      digitalWrite(BLANCO, HIGH);
+      delay(Tiempo);
+      digitalWrite(ROJO, LOW);
+      digitalWrite(AMBAR, LOW);
+      digitalWrite(VERDE, LOW);
+      digitalWrite(BLANCO, LOW);
+      delay(Tiempo);
+    }
