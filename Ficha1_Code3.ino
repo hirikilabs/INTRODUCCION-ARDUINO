d979e6b6edb7ceeca739741aef7f604899904b2e
yayitazale <surf.obsession@gmail.com>
2014-01-22 13:13:27 +0100

    Update Ficha1_Code3.ino

--- Ficha1_Code3.ino
+++ Ficha1_Code3.ino
@@ -1,4 +1,4 @@
-    const int ROJO = 11;      
+      const int ROJO = 11;      
     const int AMBAR = 10;         
     const int VERDE = 9;
     const int BLANCO = 6;
@@ -7,8 +7,8 @@
   
     void setup()
     {
-    pinMode(ROJO, OUTPUT);      // pinMode es una función que define un pin como entrada o salida. Ponemos 
-    pinMode(BLANCO, OUTPUT);    // pinMode( [el pin sobre el que queremos actuar], INPUT (entrada) / OUTPUT (salida)
+    pinMode(ROJO, OUTPUT);      // pinMode es una funciÃ³n que define un pin como entrada o salida. Ponemos 
+    pinMode(AMBAR, OUTPUT);    // pinMode( [el pin sobre el que queremos actuar], INPUT (entrada) / OUTPUT (salida)
     pinMode(VERDE, OUTPUT);
     pinMode(BLANCO, OUTPUT);
     pinMode(PULSADOR, INPUT);
@@ -30,20 +30,20 @@
       
       
      
-      while(Veces < 5) // El ciclo while funciona de tal manera que al hacer una acción mientras la condición (este caso que "Veces" sea menor a 5)
+      while(Veces < 5) // El ciclo while funciona de tal manera que al hacer una acciÃ³n mientras la condiciÃ³n (este caso que "Veces" sea menor a 5)
                        // sea cierta. La variable "Veces" recoje las veces que se ha pulsado un pulsador.
       {
         Pulso = digitalRead(PULSADOR);  // Lo mismo de antes, solo que creamos la variable "Pulso" para guardar el valor leido del pulsador 2.
-        if (Pulso == LOW)                // Si pulsamos el pulsador 2... 
-          {Veces = Veces + 1;}           // A "Veces" le sumamos 1. La primera vez, como empieza de 0, hace "Veces = 0 + 1", la segunda vuelta, "Veces" será igual a 1
-                                         // por lo tanto, hará "Veces = 1 + 1", y tomará el valor 2.
+        if (Pulso == HIGH)                // Si pulsamos el pulsador 2... 
+          {Veces = Veces + 1;}           // A "Veces" le sumamos 1. La primera vez, como empieza de 0, hace "Veces = 0 + 1", la segunda vuelta, "Veces" serÃ¡ igual a 1
+                                         // por lo tanto, harÃ¡ "Veces = 1 + 1", y tomarÃ¡ el valor 2.
      
-       // Pero... ¿cuándo acaba el bucle "while"? Esto se da cuando la condición que tiene (que "Veces" sea menor a 5) no se cumpla
+       // Pero... Â¿cuÃ¡ndo acaba el bucle "while"? Esto se da cuando la condiciÃ³n que tiene (que "Veces" sea menor a 5) no se cumpla
        // (por ejemplo en "Veces" = 5, 6...). Lo que hace es inmediatamente salir de lo que tenga dentro (el "switch" con sus case).
        
-       // "switch" es muy parecido a "if", solo que contempla más casos. Depende de la variable "Veces". Esto significa que 
-       // cuando "Veces" sea 0, el programa hará lo que ponga en "case 0:" y saldrá, cuando "Veces" sea 1, hará lo que mande case 1:
-       // y saldrá... Hasta que la condición del "while" sea falsa.
+       // "switch" es muy parecido a "if", solo que contempla mÃ¡s casos. Depende de la variable "Veces". Esto significa que 
+       // cuando "Veces" sea 0, el programa harÃ¡ lo que ponga en "case 0:" y saldrÃ¡, cuando "Veces" sea 1, harÃ¡ lo que mande case 1:
+       // y saldrÃ¡... Hasta que la condiciÃ³n del "while" sea falsa.
                                         
        
           switch(Veces)                               
